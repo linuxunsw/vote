f5be6256949511225fb091915029653b47776745
Neel Sharma <nsdj.sharma@gmail.com>
2025-08-22 22:25:59 +1000

    feat(web): add basic layout

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,4 +4,12 @@
   let { children } = $props();
 </script>
 
-{@render children?.()}
+<div class="flex min-h-screen flex-col">
+  <header class="bg-yellow-800 p-4">Vote</header>
+  <div class="container mx-auto flex grow flex-col">
+    <main class="grow space-y-4 p-4">
+      {@render children?.()}
+    </main>
+  </div>
+  <footer class="bg-neutral-800 p-4">&copy; Linux Society 2025</footer>
+</div>
