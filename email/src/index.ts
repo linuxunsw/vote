709c1fb6e1fb9b63a5cf47086600f52f5016f064
Neel Sharma <nsdj.sharma@gmail.com>
2025-08-27 19:50:14 +1000

    chore(email): remove editorconfig and reformat files
    I'm not sure what the preferred way of formatting is in the repo, so
    I'll just remove what's there in the template in case it's wrong

--- email/src/index.ts
+++ email/src/index.ts
@@ -2,25 +2,25 @@ import { EmailMessage } from "cloudflare:email";
 import { createMimeMessage } from "mimetext";
 
 export default {
-	async email(message, env, ctx) {
-		const allowList = ["nsdj.sharma@gmail.com"];
-		if (!allowList.includes(message.from)) {
-			message.setReject("Address not allowed");
-			return;
-		}
+  async email(message, env, ctx) {
+    const allowList = ["nsdj.sharma@gmail.com"];
+    if (!allowList.includes(message.from)) {
+      message.setReject("Address not allowed");
+      return;
+    }
 
-		const reply = createMimeMessage();
-		reply.setHeader("In-Reply-To", message.headers.get("Message-ID")!);
-		reply.setSender({ name: "Thank you for your contact", addr: "election@linuxunsw.org" });
-		reply.setRecipient(message.from);
-		reply.setSubject("Email test successful");
-		reply.addMessage({
-			contentType: "text/plain",
-			data: "Pong",
-		});
+    const reply = createMimeMessage();
+    reply.setHeader("In-Reply-To", message.headers.get("Message-ID")!);
+    reply.setSender({ name: "Thank you for your contact", addr: "election@linuxunsw.org" });
+    reply.setRecipient(message.from);
+    reply.setSubject("Email test successful");
+    reply.addMessage({
+      contentType: "text/plain",
+      data: "Pong",
+    });
 
-		const replyMessage = new EmailMessage("<SENDER>@example.com", message.from, reply.asRaw());
+    const replyMessage = new EmailMessage("<SENDER>@example.com", message.from, reply.asRaw());
 
-		await message.reply(replyMessage);
-	},
+    await message.reply(replyMessage);
+  },
 } satisfies ExportedHandler<Env>;
