bd3c9237311471c8981b5fa556ea7d690c569c31
l1mey112 <l-m@l-m.dev>
2025-08-28 16:38:53 +1000

    fix(tui): create new forms per instance

--- tui/internal/tui/forms/auth.go
+++ tui/internal/tui/forms/auth.go
@@ -6,22 +6,26 @@ import (
 	"github.com/linuxunsw/vote/tui/internal/tui/validation"
 )
 
-var ZIDForm = huh.NewForm(
-	huh.NewGroup(
-		huh.NewInput().
-			Key("zid").
-			Title("what's your zid?").
-			Placeholder("z5555555").
-			Validate(validation.ZID),
-	),
-).WithTheme(styles.FormTheme())
+func ZIDForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Key("zid").
+				Title("what's your zid?").
+				Placeholder("z5555555").
+				Validate(validation.ZID),
+		),
+	).WithTheme(styles.FormTheme())
+}
 
-var OTPForm = huh.NewForm(
-	huh.NewGroup(
-		huh.NewInput().
-			Title("enter verification code").
-			Key("otp").
-			Description("a code has been sent to the email associated with your zid").
-			Validate(validation.OTP),
-	),
-).WithTheme(styles.FormTheme())
+func OTPForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("enter verification code").
+				Key("otp").
+				Description("a code has been sent to the email associated with your zid").
+				Validate(validation.OTP),
+		),
+	).WithTheme(styles.FormTheme())
+}
--- tui/internal/tui/forms/nomination.go
+++ tui/internal/tui/forms/nomination.go
@@ -6,38 +6,40 @@ import (
 	"github.com/linuxunsw/vote/tui/internal/tui/validation"
 )
 
-var NominationForm = huh.NewForm(
-	huh.NewGroup(
-		huh.NewInput().
-			Key("name").
-			Title("full name").
-			Validate(validation.NotEmpty),
-		huh.NewInput().
-			Key("email").
-			Title("preferred contact email").
-			Validate(validation.Email),
-		huh.NewInput().
-			Key("discord").
-			Title("discord username").
-			Validate(validation.NotEmpty),
-		huh.NewMultiSelect[string]().
-			Key("roles").
-			Title("roles you are nominating for").
-			Options(
-				huh.NewOption("president", "president"),
-				huh.NewOption("secretary", "secretary"),
-				huh.NewOption("treasurer", "treasurer"),
-				huh.NewOption("arc delegate", "arc_delegate"),
-				huh.NewOption("edi officer", "edi_officer"),
-				huh.NewOption("grievance officer", "grievance_officer"),
-			).
-			Validate(validation.Role),
-		huh.NewText().
-			Key("statement").
-			Title("please provide a candidate statement").
-			Validate(huh.ValidateLength(50, 2000)),
-		huh.NewInput().
-			Key("url").
-			Title("url (optional)"),
-	),
-).WithTheme(styles.FormTheme())
+func NominationForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Key("name").
+				Title("full name").
+				Validate(validation.NotEmpty),
+			huh.NewInput().
+				Key("email").
+				Title("preferred contact email").
+				Validate(validation.Email),
+			huh.NewInput().
+				Key("discord").
+				Title("discord username").
+				Validate(validation.NotEmpty),
+			huh.NewMultiSelect[string]().
+				Key("roles").
+				Title("roles you are nominating for").
+				Options(
+					huh.NewOption("president", "president"),
+					huh.NewOption("secretary", "secretary"),
+					huh.NewOption("treasurer", "treasurer"),
+					huh.NewOption("arc delegate", "arc_delegate"),
+					huh.NewOption("edi officer", "edi_officer"),
+					huh.NewOption("grievance officer", "grievance_officer"),
+				).
+				Validate(validation.Role),
+			huh.NewText().
+				Key("statement").
+				Title("please provide a candidate statement").
+				Validate(huh.ValidateLength(50, 2000)),
+			huh.NewInput().
+				Key("url").
+				Title("url (optional)"),
+		),
+	).WithTheme(styles.FormTheme())
+}
