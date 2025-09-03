f4e96221d23852a8cc4d622703e74202887e05cc
suwuako <ronnielow@protonmail.com>
2025-09-04 00:27:25 +1000

    feat(email): initialise cf worker skeleton code

--- email/src/index.ts
+++ email/src/index.ts
@@ -1,26 +1,35 @@
 import { EmailMessage } from "cloudflare:email";
-import { createMimeMessage } from "mimetext";
+import * as PostalMime from 'postal-mime';
+import { createMimeMessage } from 'mimetext';
 
+/*
+  “name” (string) - The nominee’s full name
+  “contactEmail” (string) (default should be zID email -> up to the client) - The nominee’s contact email
+  “statement” (string) - The nominee’s candidate statement
+  “roles” (Role array) - An array of the roles (role keys) the nominee is running for
+  “discord” (string) - The nominee’s Discord username
+  “url” (string) (optional)
+*/
 export default {
   async email(message, env, ctx) {
-    const allowList = ["nsdj.sharma@gmail.com"];
-    if (!allowList.includes(message.from)) {
-      message.setReject("Address not allowed");
-      return;
-    }
+    const parser = new PostalMime.default();
+    const rawEmail = new Response(message.raw);
+    const email = await parser.parse(await rawEmail.arrayBuffer());
+    const emailContent = email.html
+  },
 
-    const reply = createMimeMessage();
-    reply.setHeader("In-Reply-To", message.headers.get("Message-ID")!);
-    reply.setSender({ name: "Thank you for your contact", addr: "election@linuxunsw.org" });
-    reply.setRecipient(message.from);
-    reply.setSubject("Email test successful");
-    reply.addMessage({
-      contentType: "text/plain",
-      data: "Pong",
+  async fetch(request, env, ctx) {
+    const msg = createMimeMessage();
+    msg.setSender({ name: 'Sending email test', addr: 'sender@example.com' });
+    msg.setRecipient('recipient@example.com');
+    msg.setSubject('An email generated in a worker');
+    msg.addMessage({
+      contentType: 'text/plain',
+      data: `Congratulations, you just sent an email from a worker.`,
     });
 
-    const replyMessage = new EmailMessage("<SENDER>@example.com", message.from, reply.asRaw());
-
-    await message.reply(replyMessage);
-  },
-} satisfies ExportedHandler<Env>;
+    var message = new EmailMessage('sender@example.com', 'recipient@example.com', msg.asRaw());
+    await env.EMAIL.send(message);
+    return Response.json({ ok: true });
+  }
+};
