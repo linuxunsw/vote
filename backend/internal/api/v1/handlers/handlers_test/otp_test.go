d40852d11824f88ee3f6ccaf136fe9a5d2e4b6f0
l-m <l-m@l-m.dev>
2025-09-11 09:08:00 +1000

    fix(otp): bugfix on admins being unable to login without an election present (#49)
    * fix(elections): SQL dropping the index on down
    
    * feat(otp): handling admins by `ADMIN_ZIDS` env variable
    
    * feat(nomination): implement nominations
    
    * fix(election-nomination): add more tests
    
    * fix(otp): move tests around
    
    * feat(cmd): goose command line migrations
    
    * fix(otp): OTP handle edge cases around admin, create test cases

--- backend/internal/api/v1/handlers/handlers_test/otp_test.go
+++ backend/internal/api/v1/handlers/handlers_test/otp_test.go
@@ -11,7 +11,7 @@ func TestOTPConsumeMember(t *testing.T) {
 	api, mailer := NewAPI(t)
 
 	zid := "z0000000"
-	_ = createElection(t, api, cfg.JWT, []string{
+	_ = createElection(t, api, cfg.JWT, TestingDummyJWTAdmin, []string{
 		zid,
 	})
 
@@ -30,11 +30,29 @@ func TestOTPConsumeNonMember(t *testing.T) {
 	cfg := config.Load()
 	api, mailer := NewAPI(t)
 
-	_ = createElection(t, api, cfg.JWT, []string{})
+	_ = createElection(t, api, cfg.JWT, TestingDummyJWTAdmin, []string{})
 
 	zid := "z0000000"
 	resp := generateOTPSubmit(t, api, mailer, zid)
 	if resp.Code != 403 {
 		t.Fatalf("expected 403 Forbidden, got %d", resp.Code)
 	}
+}
+
+func TestOTPAdmins(t *testing.T) {
+	t.Setenv("ADMIN_ZIDS", "z0000000,z0000001,z0000002")
+
+	zid := "z0000001"
+	
+	cfg := config.Load()
+	api, mailer := NewAPI(t)
+
+	resp := generateOTPSubmit(t, api, mailer, zid)
+	res := resp.Result()
+
+	if res.Header.Get("Set-Cookie") == "" {
+		t.Fatalf("expected Set-Cookie header, got none")
+	}
+	jwt := extractJWT(res.Header)
+	_ = createElection(t, api, cfg.JWT, jwt, []string{})
 }
