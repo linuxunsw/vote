6a8bab09b705a7b3d3f33b94e2de0ff2f335061b
suwuako <ronnielow@protonmail.com>
2025-09-15 18:07:07 +1000

    feat: reset wrangler, change email client to resopnd on email

--- email/src/index.ts
+++ email/src/index.ts
@@ -1,35 +1,21 @@
-import { EmailMessage } from "cloudflare:email";
 import * as PostalMime from 'postal-mime';
+import { EmailMessage } from "cloudflare:email";
 import { createMimeMessage } from 'mimetext';
 
-/*
-  “name” (string) - The nominee’s full name
-  “contactEmail” (string) (default should be zID email -> up to the client) - The nominee’s contact email
-  “statement” (string) - The nominee’s candidate statement
-  “roles” (Role array) - An array of the roles (role keys) the nominee is running for
-  “discord” (string) - The nominee’s Discord username
-  “url” (string) (optional)
-*/
 export default {
   async email(message, env, ctx) {
-    const parser = new PostalMime.default();
-    const rawEmail = new Response(message.raw);
-    const email = await parser.parse(await rawEmail.arrayBuffer());
-    const emailContent = email.html
-  },
-
-  async fetch(request, env, ctx) {
     const msg = createMimeMessage();
-    msg.setSender({ name: 'Sending email test', addr: 'sender@example.com' });
-    msg.setRecipient('recipient@example.com');
+    msg.setSender({ name: 'Thank you for your contact', addr: 'sender@example.com' });
+    msg.setRecipient(message.from);
+    msg.setHeader('In-Reply-To', message.headers.get('Message-ID'));
     msg.setSubject('An email generated in a worker');
     msg.addMessage({
       contentType: 'text/plain',
-      data: `Congratulations, you just sent an email from a worker.`,
+      data: `This is an automated reply. We received your email.`,
     });
 
-    var message = new EmailMessage('sender@example.com', 'recipient@example.com', msg.asRaw());
-    await env.EMAIL.send(message);
-    return Response.json({ ok: true });
-  }
+    const replyMessage = new EmailMessage('sender@example.com', message.from, msg.asRaw());
+
+    await message.reply(replyMessage);
+  },
 };
