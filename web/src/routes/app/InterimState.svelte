aff6a5d23d910a0fd1935e6de3094a35d230f50c
Neel Sharma <nsdj.sharma@gmail.com>
2025-09-20 21:37:20 +1000

    feat(web): add preliminary ui for interim (NOMINATIONS_CLOSED and VOTING_CLOSED) states

---
+++ web/src/routes/app/InterimState.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { LoaderCircle } from "@lucide/svelte";
+
+  type Props = {
+    message: string;
+  };
+
+  let { message }: Props = $props();
+</script>
+
+<div class="grid h-full">
+  <div class="space-y-2 place-self-center text-center">
+    <div class="flex w-full justify-center">
+      <LoaderCircle size={54} class="animate-spin" />
+    </div>
+    <h2 class="text-2xl font-bold">{message}</h2>
+    <h3 class="text-lg">
+      in the meantime, wanna follow our <a
+        class="text-blue-500 underline"
+        href="https://instagram.com/linsocunsw">instagram</a
+      >?
+    </h3>
+  </div>
+</div>
