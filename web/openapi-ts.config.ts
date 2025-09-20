17011af2bb6715dad645b35aa99c54738913fdb5
fabledsaint <35288585+fabledsaint@users.noreply.github.com>
2025-09-20 19:02:44 +1000

    feat(web): added validator, schema, transformer generation to api client. implemented ignore rule for generated files in eslint

--- web/openapi-ts.config.ts
+++ web/openapi-ts.config.ts
@@ -2,5 +2,28 @@ import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   input: "http://localhost:8888/openapi.yaml",
-  output: "./src/lib/api",
+  output: {
+    path: "src/lib/api",
+    format: "prettier",
+    lint: "eslint",
+  },
+  plugins: [
+    "@hey-api/schemas",
+    {
+      dates: true,
+      name: "@hey-api/transformers",
+    },
+    {
+      enums: "javascript",
+      name: "@hey-api/typescript",
+    },
+    "zod",
+    {
+      name: "@hey-api/sdk",
+      validator: {
+        request: true,
+      },
+      transformer: true,
+    },
+  ],
 });
