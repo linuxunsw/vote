00c10147c71b692761e87847f9486a2f3b103996
fabledsaint <35288585+fabledsaint@users.noreply.github.com>
2025-09-20 20:53:44 +1000

    feat(web): implment inital dark mode with yellow theme

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
   import SEO from "$lib/components/SEO.svelte";
+  import ModeToggle from "$lib/components/mode-toggle.svelte";
 
   let { children } = $props();
 </script>
 
+<ModeWatcher />
+
 <SEO title="LinSoc Vote" />
 
 <div class="flex min-h-screen flex-col">
-  <header class="bg-yellow-500 p-4 font-bold">Vote</header>
+  <header class="p-4 font-bold">Vote</header>
   <div class="mx-auto w-full grow space-y-4 p-4 md:w-2/3">
     {@render children?.()}
   </div>
-  <footer class="bg-neutral-300 p-4">&copy; Linux Society 2025</footer>
+  <footer class="flex flex-row content-between items-center justify-center gap-4 p-4">
+    <p>Linux Society 2025</p>
+    <ModeToggle />
+  </footer>
 </div>
