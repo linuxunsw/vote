ebc4d1e5be5c8565773cf92e65cd3bf88075948f
Neel Sharma <nsdj.sharma@gmail.com>
2025-09-20 18:00:35 +1000

    feat(web): actually add a title and some meta information

---
+++ web/src/lib/components/SEO.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  type Props = {
+    title: string;
+  };
+
+  let { title }: Props = $props();
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta
+    name="description"
+    content="Vote is an application that manages and holds elections for UNSW Linux Society."
+  />
+  <meta name="og:title" content={title} />
+</svelte:head>
