5509d82b5fec48c08ca75289bb0a69a88f15ffa8
Neel Sharma <nsdj.sharma@gmail.com>
2025-09-20 17:54:39 +1000

    feat(web): add confirmation popover for nomination revocation

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex min-h-screen flex-col">
   <header class="bg-yellow-500 p-4 font-bold">Vote</header>
-  <div class="container mx-auto grow space-y-4 p-4">
+  <div class="mx-auto w-full grow space-y-4 p-4 md:w-2/3">
     {@render children?.()}
   </div>
   <footer class="bg-neutral-300 p-4">&copy; Linux Society 2025</footer>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import LoginCard from "./LoginCard.svelte";
 </script>
 
-<div class="mx-auto grid h-full md:max-w-2/3">
+<div class="grid h-full">
   <div
     class="grid grid-rows-[1fr_auto_1fr] gap-5 place-self-center md:grid-cols-[1fr_auto_1fr] md:grid-rows-1"
   >
