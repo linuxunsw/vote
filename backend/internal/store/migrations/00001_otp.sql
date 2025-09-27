4e4208d4c3d3a99a34c6cff2b91d246ec56e0bc0
Zopolis4 <creatorsmithmdt@gmail.com>
2025-09-27 21:09:55 +1000

    chore(backend): Apply sqlfluff to backend/internal/store/migrations/*.sql

--- backend/internal/store/migrations/00001_otp.sql
+++ backend/internal/store/migrations/00001_otp.sql
@@ -1,10 +1,10 @@
 -- +goose Up
-create table "otp" (
-    "zid" text primary key,
-    "code_hash" text not null,
-    "retry_amount" integer not null default 0,
-    "created_at" timestamp (3) with time zone not null
+create table otp (
+    zid text primary key,
+    code_hash text not null,
+    retry_amount integer not null default 0,
+    created_at timestamp (3) with time zone not null
 );
 
 -- +goose Down
-drop table "otp";
+drop table otp;
